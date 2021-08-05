5aa047bbed8912560c8706d57a9ea81e73261921
NgTuanLoc <18521011@gm.uit.edu.vn>
2021-08-05 14:46:09 +0700

    complete homepage

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,9 +1,11 @@
 import { Card } from "react-bootstrap";
+import Rating from "./Rating";
+import PropsTypes from 'prop-types'
 
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
-      <a href={`product/${product._id}`}>
+      <a href={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top"></Card.Img>
       </a>
       <Card.Body>
@@ -15,13 +17,27 @@ const Product = ({ product }) => {
 
         <Card.Text as="div">
           <div className="my-3">
-            {product.rating} From {product.numReviews} Reviews
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} reviews`}
+
+            />
           </div>
         </Card.Text>
 
-        <Card.Text as="h3">${product.price }</Card.Text>
+        <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
     </Card>
   );
 };
+
+Rating.defaultProps = {
+    color: '# F5C71B' 
+}
+
+Rating.propTypes = {
+    text: PropsTypes.string.isRequired,
+    value: PropsTypes.number.isRequired,
+    color: PropsTypes.string
+}
 export default Product;
